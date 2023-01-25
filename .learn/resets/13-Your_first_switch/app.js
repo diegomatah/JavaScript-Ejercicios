6d2028f6a754196f76c11521fe325485fb58288f
diegomatah <diegoa.matah@gmail.com>
2023-01-25 01:18:27 +0000

    js3

--- .learn/resets/13-Your_first_switch/app.js
+++ .learn/resets/13-Your_first_switch/app.js
@@ -2,17 +2,12 @@ function getColor(selection)
 {
 	switch(selection){
 		// Add more options here
-		case 'red':
-			return true;
-		case 'green':
-			return true;
-		case 'blue':
-			return true;
 	    default:
 	    	return false;  //returns false because the user picked an unavailable color
 	    break;               
 	}
 }
+
 let colorname = prompt('What color do you want?').trim();
 let isAvailable = getColor(colorname);
 
