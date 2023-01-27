dd5c163201c30dfb6bc5752a0e74da43357b76d3
diegomatah <diegoa.matah@gmail.com>
2023-01-27 21:38:30 +0000

    js6

--- exercises/17-Create_a_for_loop/app.js
+++ exercises/17-Create_a_for_loop/app.js
@@ -1,4 +1,10 @@
-// Declare and write your function here:
+function standardsMaker() {
+    // Declare and write your function here:
+    for (let index = 0; index < 300; index++) {
+        console.log ("I will write questions if I'm stuck");
+        
+    }
+}
 
 
 standardsMaker();
